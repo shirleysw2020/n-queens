08c0848ae6cec275d4f1e771410cc53e99462d2e
Jaycie Kassahn <jckassahn@gmail.com>
2020-06-30 21:35:02 -0700

    Complete test definitions for 'contains', 'every', 'some, and 'extend'

--- spec/part2.js
+++ spec/part2.js
@@ -57,8 +57,10 @@
 
       it('should return false given an array and a value not in that array', function() {
         // Replace this line with an `expect` statement that tests
+        var array = [1, 2, 3];
+        var value = 6;
+        expect(_.contains(array, value)).to.be.false;
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
       });
 
       it('should return true given a object and a value from that object', function() {
@@ -91,7 +93,7 @@
       it('fails for a collection of all-falsy values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.every([false, null, 0], _.identity)).to.be.false;
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
@@ -149,7 +151,7 @@
       it('should fail for a set containing no matching values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.some([3, 15, 17], isEven)).to.be.false;
       });
 
       it('should pass for a collection containing one matching value', function() {
@@ -189,7 +191,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { x: 'x' };
+        var source = { x: 'b' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.x).to.equal('b');
       });
 
       it('should not override properties not found in the source', function() {
@@ -413,7 +419,7 @@
         memoSpy(10);
         expect(spy).to.have.been.calledOnce;
       });
-      
+
       it('should not run the memoized function twice when given a reference type as an argument', function() {
         // Be careful how you are checking if a set of arguments has been passed in already
         var spy = sinon.spy(function() { return 'Dummy output'; });
@@ -473,7 +479,7 @@
         expect(shuffled).to.not.equal(numbers);
         expect(numbers).to.eql([4, 5, 6]);
       });
-            
+
       it('should maintain same array length', function() {
         var numbers = [1, 1, 2, 3];
         var shuffled = _.shuffle(numbers);
